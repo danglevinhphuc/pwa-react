03abb30ba763e8ec37141389a044472a068fc967
phucdlv <phucdlv@twin.vn>
2020-01-07 09:55:42 +0700

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name][chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: path.join(__dirname, "."),
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/react",
+            {
+              plugins: ["@babel/plugin-proposal-class-properties"]
+            }
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/public",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: "./public"
+  }
+};
